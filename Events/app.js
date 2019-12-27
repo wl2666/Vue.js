07d34ab4117e6f992aa5cd4398d94f3b74241f96
Vicky <wl2666@tc.columbia.edu>
2019-12-27 16:29:29 -0500

    Events

---
+++ Events/app.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		description: 'Type any color name, rgb, hex or hsl in the input below and see what happens!',
+		bgc: {
+			backgroundColor: ''
+		}
+	}
+})
